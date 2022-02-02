ba43a379bf04a593ed984f82026c59f81e5c92ae
Nanaka-spec <bimowicaksono138@gmail.com>
2022-02-02 11:03:09 +0700

    heroku

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "OTM4MjY4Mzk5OTMzNDgxMDAx.Yfn0ig.Of4jzG2qRJ5u-fSLPFgSLBiOUCs", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "938268399933481001", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "b8-lW-Eon6Lpqka_wbhrNzjWJKBv1QTY", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
@@ -17,7 +17,7 @@ module.exports = {
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205281600, //Bot Inviting Permissions
-  Website: process.env.Website || "http://changeme", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  Website: process.env.Website || "0.0.0.0", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
@@ -28,15 +28,15 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
-    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    host: "lavalink-replit.bimo-satriosatr.repl.co",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "maybeiwasboring",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "95f2412e9f8a4b6288716c1a3f013c9c", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "90da50c98465472cb4b385a71318f7f7", //Spotify Client Secret
   },
 };
